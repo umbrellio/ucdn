9eb35b3e450015dc4bfebeda14e7ec83bf6d3743
Yuri Smirnov <tycooon@yandex.ru>
2021-07-14 18:08:42 +0300

    add eslint rules

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const path = require("path")
 const fs = require("fs")
 const yaml = require("js-yaml")
 
-const toDashCase = str => str.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`);
+const toDashCase = str => str.replace(/([A-Z])/g, g => `-${g[0].toLowerCase()}`)
 
 const loadYamlFile = filePath => {
   const configPath = path.resolve(filePath)
