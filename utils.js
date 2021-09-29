6f89a458afbbc9fdc175661511f45ed396b412af
Aleksei <nulldefiner@gmail.com>
2021-09-29 13:39:59 +0300

    update deps

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ const toDashCase = str => str.replace(/([A-Z])/g, g => `-${g[0].toLowerCase()}`)
 const loadYamlFile = filePath => {
   const configPath = path.resolve(filePath)
   const content = fs.readFileSync(configPath)
-  return yaml.safeLoad(content)
+  return yaml.load(content)
 }
 
 module.exports = {
